d013afb61da81ab8d84d6598bc3089456fd5aeef
Peter Reutemann <fracpete@gmail.com>
2023-10-19 10:15:38 +1300

    updated github URLs

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     long_description=(
             _read('DESCRIPTION.rst') + b'\n' +
             _read('CHANGES.rst')).decode('utf-8'),
-    url="https://github.com/waikato-datamining/ldc-openai",
+    url="https://github.com/waikato-llm/ldc-openai",
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
